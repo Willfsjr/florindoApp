12a5703359265cb073e66231cf97f831c30a92a5
Willian Ferreira <willianfsjr@gmail.com>
2025-02-11 17:58:33 -0400

    iniciando configuração backend

--- backend/src/models/prisma.js
+++ backend/src/models/prisma.js
@@ -1,4 +1,11 @@
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
+import { PrismaClient } from "@prisma/client";
 
-module.exports = prisma;
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
+
+export default prisma;
